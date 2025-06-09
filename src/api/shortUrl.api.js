4be0911df2359fb31cd0a5b8c1042e687150af7a
Pratyush <choudharypratyush809@gmail.com>
2025-06-09 18:16:26 +0530

    fixed shorturl

--- src/api/shortUrl.api.js
+++ src/api/shortUrl.api.js
@@ -2,11 +2,11 @@ import axiosInstance from "../utils/axiosInstance";
 
 
 const createShortUrl = async (url, slug) => {
-    const { data } = await axiosInstance.post("/api/v1/shorturl/create-shorturl-auth", {url, slug})
+    const { data } = await axiosInstance.post("/create-shorturl-auth", {url, slug})
     return data;
 }
 const getRecentUrls = async ( ) => {
-    const response = await axiosInstance.get("/api/v1/shorturl/recenturls")
+    const response = await axiosInstance.get("/recenturls")
     return response.data;
 }
 
