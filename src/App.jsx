ed8aa607230e3cce9d1ac30779b9b337ec3236de
Pratyush <choudharypratyush809@gmail.com>
2025-06-09 17:27:39 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Registerform />} />
         <Route
-          path="/"
+          path="/dashboard"
           element={
             // <PrivateRoute>
               <Dashboardpage />
